6cbbf3aee048e075d46db332d7bf7a1aacc7814f
jenryEduardo <eduardogomezcalvo851@gmail.com>
2025-07-23 20:37:56 -0600

    finish all

--- MPU/infraestructure/http/MySQL.go
+++ MPU/infraestructure/http/MySQL.go
@@ -19,13 +19,9 @@ func NewMYSQLRepository(db *sql.DB) *MYSQLRepository {
 // Guarda un nuevo registro en la tabla mpu
 func (r *MYSQLRepository) Save(mpu domain.Mpu) error {
 	tiempo := time.Now()
-	query := `INSERT INTO mpu (aceleracion_x, aceleracion_y, aceleracion_z, pasos, nivel_actividad,fecha) VALUES (?, ?, ?, ?, ?, ?)`
+	query := `INSERT INTO mpu (pasos,fecha) VALUES (?, ?)`
 	_, err := r.db.Exec(query,
-		mpu.Mpu6050.Aceleracion.X,
-		mpu.Mpu6050.Aceleracion.Y,
-		mpu.Mpu6050.Aceleracion.Z,
-		mpu.Pasos,
-		mpu.NivelActividad,
+		&mpu.Pasos,
 		tiempo,
 	)
 	return err
@@ -33,13 +29,9 @@ func (r *MYSQLRepository) Save(mpu domain.Mpu) error {
 
 // Actualiza un registro por id
 func (r *MYSQLRepository) UpdateByID(id int, mpu domain.Mpu) error {
-	query := `UPDATE mpu SET aceleracion_x=?, aceleracion_y=?, aceleracion_z=?, pasos=?, nivel_actividad=? WHERE id = ?`
+	query := `UPDATE mpu SET pasos=? WHERE id = ?`
 	result, err := r.db.Exec(query,
-		mpu.Mpu6050.Aceleracion.X,
-		mpu.Mpu6050.Aceleracion.Y,
-		mpu.Mpu6050.Aceleracion.Z,
 		mpu.Pasos,
-		mpu.NivelActividad,
 		id,
 	)
 
@@ -69,7 +61,7 @@ func (r *MYSQLRepository) DeleteByID(id int) error {
 
 // Obtiene todos los registros
 func (r *MYSQLRepository) FindAll() ([]domain.Mpu, error) {
-	query := `SELECT id, pasos, nivel_actividad,fecha FROM mpu`
+	query := `SELECT id, pasos,fecha FROM mpu`
 	rows, err := r.db.Query(query)
 	if err != nil {
 		return nil, err
@@ -79,7 +71,7 @@ func (r *MYSQLRepository) FindAll() ([]domain.Mpu, error) {
 	var MPUs []domain.Mpu
 	for rows.Next() {
 		var m domain.Mpu
-		err := rows.Scan(&m.Id, &m.Pasos, &m.NivelActividad,&m.Fecha)
+		err := rows.Scan(&m.Id, &m.Pasos,&m.Fecha)
 		if err != nil {
 			return nil, err
 		}
@@ -91,7 +83,7 @@ func (r *MYSQLRepository) FindAll() ([]domain.Mpu, error) {
 
 // Obtiene registros por id (aunque suele ser solo uno, retorna slice por consistencia)
 func (r *MYSQLRepository) FindByID(id int) ([]domain.Mpu, error) {
-	query := `SELECT id, aceleracion_x, aceleracion_y, aceleracion_z, pasos, nivel_actividad FROM mpu WHERE id = ?`
+	query := `SELECT id, pasos, fecha FROM mpu WHERE id = ?`
 	rows, err := r.db.Query(query, id)
 	if err != nil {
 		return nil, err
@@ -101,7 +93,7 @@ func (r *MYSQLRepository) FindByID(id int) ([]domain.Mpu, error) {
 	var MPUs []domain.Mpu
 	for rows.Next() {
 		var m domain.Mpu
-		err := rows.Scan(&m.Id, &m.Mpu6050.Aceleracion.X, &m.Mpu6050.Aceleracion.Y, &m.Mpu6050.Aceleracion.Z, &m.Pasos, &m.NivelActividad)
+		err := rows.Scan(&m.Id, &m.Pasos,&m.Fecha)
 		if err != nil {
 			return nil, err
 		}
--- MPU/infraestructure/http/controllers/saveMPUController.go
+++ MPU/infraestructure/http/controllers/saveMPUController.go
@@ -2,11 +2,8 @@ package controllers
 
 import (
 	"API-VITALVEST/MPU/application"
-	"API-VITALVEST/MPU/infraestructure/control"
-
 	"API-VITALVEST/MPU/domain"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,26 +16,26 @@ func NewSaveMPUController(uc *application.SaveMPU_UC) *SaveMPUController {
 }
 
 func (ctrl *SaveMPUController) Run(c *gin.Context) {
-	var input struct {
-		Mpu6050 domain.Mpu6050 `json:"mpu6050"`
-	}
 
-	if err := c.ShouldBindJSON(&input); err != nil {
+		var data  domain.Mpu 
+	
+
+	if err := c.ShouldBindJSON(&data); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Datos inválidos o incompletos"})
 		return
 	}
 
-	// Calcular pasos y nivel con aceleracion recibida
-	pasos, nivel := control.ConvertirDatosEnPasos(input.Mpu6050.Aceleracion.X, input.Mpu6050.Aceleracion.Y, input.Mpu6050.Aceleracion.Z)
+	// // Calcular pasos y nivel con aceleracion recibida
+	// pasos, nivel := control.ConvertirDatosEnPasos(input.Mpu6050.Aceleracion.X, input.Mpu6050.Aceleracion.Y, input.Mpu6050.Aceleracion.Z)
 
-	// Crear objeto para guardar
-	mpu := domain.Mpu{
-		Mpu6050:       input.Mpu6050,
-		Pasos:         pasos,
-		NivelActividad: nivel,
-	}
+	// // Crear objeto para guardar
+	// mpu := domain.Mpu{
+	// 	Mpu6050:       input.Mpu6050,
+	// 	Pasos:         pasos,
+	// 	NivelActividad: nivel,
+	// }
 
-	err := ctrl.uc.Run(mpu)
+	err := ctrl.uc.Run(data)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
@@ -49,9 +46,7 @@ func (ctrl *SaveMPUController) Run(c *gin.Context) {
 		"data": gin.H{
 			"type": "MPU",
 			"attributes": gin.H{
-				"mpu6050":        mpu.Mpu6050,
-				"pasos":          mpu.Pasos,
-				"nivel_actividad": mpu.NivelActividad,
+				"pasos":          data.Pasos,
 			},
 		},
 	})
