0b36c9e65e956bda4e193ed79a3a69d7ee635f33
diedgo05 <233358@ids.upchiapas.edu.mx>
2025-12-08 23:28:59 -0600

    prueba de concurrencia en bme

--- BME/application/getAllBME.go
+++ BME/application/getAllBME.go
@@ -1,15 +1,34 @@
 package application
 
-import "API-VITALVEST/BME/domain"
+import (
+	"context"
+	"time"
+)
 
-type GetAllBME_UC struct {
-	db domain.IBme
-}
+func (uc *GetAllBME_UC) Run(ctx context.Context) ([]domain.Bme, error) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
 
-func NewGetAllBME_UC(db domain.IBme) *GetAllBME_UC {
-	return &GetAllBME_UC{db: db}
-}
+	resultChan := make(chan []domain.Bme)
+	errChan := make(chan error)
 
-func (uc *GetAllBME_UC) Run() ([]domain.Bme, error) {
-	return uc.db.FindAll()
+	go func() {
+		data, err := uc.db.FindAll()
+		if err != nil {
+			errChan <- err
+			return
+		}
+		resultChan <- data
+	}()
+
+	select {
+	case res := <-resultChan:
+		return res, nil
+
+	case err := <-errChan:
+		return nil, err
+
+	case <-ctx.Done():
+		return nil, ctx.Err() // timeout exceeded
+	}
 }
